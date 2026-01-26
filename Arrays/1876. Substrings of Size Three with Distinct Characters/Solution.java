0686aefd230cceadbb0521976ab8005e99ac0565
anshsharma8 <143491962+anshsharma8@users.noreply.github.com>
2026-01-27 01:09:34 +0530

    Create Solution.java

---
+++ Arrays/1876. Substrings of Size Three with Distinct Characters/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countGoodSubstrings(String s) {
+        int count = 0;
+
+        if (s.length() < 3) {
+            return 0;
+        }
+
+        for (int i = 2; i < s.length(); i++) {
+            if (s.charAt(i) != s.charAt(i - 2) &&
+                s.charAt(i) != s.charAt(i - 1) &&
+                s.charAt(i - 2) != s.charAt(i - 1)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
